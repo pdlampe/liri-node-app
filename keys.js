13195b9855120b5380654ec2001eb3516f24f155
Paul David Lampe <pdlampe@gmail.com>
2019-08-07 17:21:36 -0500

    Initial Spotify Code

--- keys.js
+++ keys.js
@@ -1,6 +1,6 @@
 console.log('this is loaded');
 
 exports.spotify = {
-    id: process.env.SPOTIFY_ID,
-    secret: process.env.SPOTIFY_SECRET
+    id: b938ca0d8b93419ba35ae58377ba161c,
+    secret: 0d59c3a0f54a4f92b4fb1bfc58226850
 };
---
+++ liri.js
@@ -0,0 +1,40 @@
+require("dotenv").config();
+
+var keys = require("./keys.js");
+var Spotify = require('node-spotify-api');
+var log = require('simple-node-log').createSimpleFileLogger(filename); log.setLevel('all');
+
+var firstCommand = process.argv[2];
+var secondCommand = process.argv[3];
+
+for (var i = 4; i < process.argv.length; i++) {
+    secondCommand += '+' + process.argv[i];
+}
+
+var spotify = new Spotify(keys.spotify);
+
+var getArtistInfo = function (artist) {
+    return artist.name;
+    console.log(artist.name)
+}
+
+var getSpotify = function (songName) {
+    if (songName === undefined) {
+        songName = "Take on me";
+    }
+
+    spotify.search(
+        {
+            type: "track",
+            query: firstCommand
+        },
+        function (err, data) {
+            if (err) {
+                console.log("The following error occured:" + err);
+                return;
+            }
+
+        }
+
+
+    )
