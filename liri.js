ada8af499f6fe84a1008119bf196c4f56fa90453
Paul David Lampe <pdlampe@gmail.com>
2019-08-07 21:13:40 -0500

    OMDB API included

--- liri.js
+++ liri.js
@@ -5,6 +5,7 @@ var Spotify = require('node-spotify-api');
 
 
 var firstCommand = process.argv[2];
+var secondCommand = process.argv[3];
 
 for (var i = 4; i < process.argv.length; i++) {
     secondCommand += '+' + process.argv[i];
@@ -35,13 +36,12 @@ var getSpotify = function (songName) {
 
             for (var i = 0; i < songs.length; i++) {
                 console.log(i);
-                console.log("Artist(s): " + songs[i].artists.map(getArtistNames));
+                console.log("Artist(s): " + songs[i].artists.map(getArtistInfo));
                 console.log("Song name: " + songs[i].name);
                 console.log("Preview song: " + songs[i].preview_url);
                 console.log("Album: " + songs[i].album.name);
                 console.log("-----------------------------------");
             }
-
 
         }
 
@@ -51,18 +51,59 @@ var getSpotify = function (songName) {
 
 function userSwitch(userCommand) {
     switch (userCommand) {
+
+        case "concert-this":
+            findConcert();
+            break;
         case "spotify-this-song":
-            getSpotify();
+            gotoSpotify();
             break;
 
         case "movie-this":
-            getMovie();
+            findMovie();
             break;
 
         case "do-what-it-says":
-            doWhat();
+            doCommand();
             break;
 
     }
 
+};
+
+function findMovie() {
+
+    var movieName = secondCommand;
+
+    var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&tomatoes=true&apikey=trilogy";
+
+    request(queryUrl, function (error, response, body) {
+        if (!error && response.statusCode === 200) {
+            var body = JSON.parse(body);
+            console.log("Title: " + body.Title);
+            console.log("Release date: " + body.Year);
+            console.log("Rotten Tomatoes Rating: " + body.Ratings[2].Value);
+            console.log("IMdB Rating: " + body.imdbRating);
+            console.log("Country: " + body.Country);
+            console.log("Plot: " + body.Plot);
+            console.log("Actors: " + body.Actors);
+            console.log("Language: " + body.Language);
+
+
+
+        }
+        else {
+
+            console.log("Error")
+        }
+        if (movieName === "Mr. Nobady") {
+            console.log("If you haven't watched 'Mr. Nobody,' then you should: http://www.imdb.com/title/tt0485947/");
+            console.log("It's on Netflix!");
+        }
+
+    });
+
 }
+
+
+mySwitch(userCommand);
